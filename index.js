9c95091d3c2129fc869a4cbf558e7b5a51b14926
akshaymewara <akshay.mewara@habilelabs.io>
2022-04-07 13:44:07 +0530

    video stream assignment

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from 'express';
+import path from 'path';
+import fs from 'fs';
+const __dirname = path.resolve();
+const app = express();
+
+app.get("/", function (req, res) {
+    res.sendFile("/index.html",{root: __dirname});
+});
+
+app.get("/video", function (req, res) {
+    const range = req.headers.range;
+    if (!range) {
+        res.status(400).send("Requires Range header");
+    }
+    const videoPath = "video.mp4";
+    const videoSize = fs.statSync("video.mp4").size;
+
+    const CHUNK_SIZE = 10 ** 4;
+    const start = Number(range.replace(/\D/g, ""));
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+    const contentLength = end - start + 1;
+    const headers = {
+        "Content-Range": `bytes ${start} - ${end}/${videoSize}`,
+        "Accept-Ranges": "bytes",
+        "Content-Length": contentLength,
+        "Content-Type": "video/mp4",
+    };
+
+    res.writeHead(206, headers);
+
+    const videoStream = fs.createReadStream(videoPath, { start, end });
+
+    videoStream.pipe(res);
+});
+
+
+
+
+
+
+app.listen(8000, function () {
+    console.log("Listening on port 8000!");
+});
+
