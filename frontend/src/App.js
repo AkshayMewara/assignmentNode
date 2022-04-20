dbc4fe23495df6990719d6cc47b0ad51930679d0
akshaymewara <akshay.mewara@habilelabs.io>
2022-04-20 15:14:57 +0530

    nodemailer frontend backend assignment

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,38 +1,36 @@
 import axios from "axios";
 import React, { useState } from "react"
 
-import './App.css';
-
 function App() {
-const [ sent, setSent ] = useState(false)
-const [ text, setText ] = useState("")
+  const [sent, setSent] = useState(false)
+  const [text, setText] = useState("")
 
-const handleSend = async() => {
-  // setSent(true)
-  try {
-    setSent(true);
-    await axios.post("http://localhost:4000/api/v1/send-email",
-    {
-      text,
-    });
-  }
-  catch(error){
-    console.log(error)
-  }
-};
+  const handleSend = async () => {
+    // setSent(true)
+    try {
+      setSent(true);
+      await axios.post("http://localhost:4000/api/v1/send-email",
+        {
+          text,
+        });
+    }
+    catch (error) {
+      console.log(error)
+    }
+  };
 
   return (
     <div className="App">
-      { !sent ?  (
-      <form onSubmit={handleSend}>
-        <input type="text" value={text} onChange={(e) => setText(e.target.value) }/>
-        <button type="submit">Send</button>
-      </form>
+      {!sent ? (
+        <form onSubmit={handleSend}>
+          <input type="text" value={text} onChange={(e) => setText(e.target.value)} />
+          <button type="submit">Send</button>
+        </form>
       ) : (
-          <h1>Email Sent</h1>
+        <h1>Email Sent</h1>
       )
-    }
-    
+      }
+
 
     </div>
   );
